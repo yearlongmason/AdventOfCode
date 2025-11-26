281e850b27937b26f7b504dafbb21d0d30eb80dc
yearlongmason <mdudelee@gmail.com>
2025-11-26 15:18:45 -0500

    Finished Go day 1 part 2!

--- 2015/Day01/Day01Code.go
+++ 2015/Day01/Day01Code.go
@@ -19,9 +19,30 @@ func getInput(fileName string) string {
 func part1() {
 	instructions := getInput("SantaInstructions.txt")
 	finalFloor := strings.Count(instructions, "(") - strings.Count(instructions, ")")
-	fmt.Printf("The instructions take Santa to floor %d", finalFloor)
+	fmt.Printf("The instructions take Santa to floor %d\n", finalFloor)
+}
+
+func part2() {
+	instructions := getInput("SantaInstructions.txt")
+	currentFloor := 0
+
+	// Loop through each instruction
+	for index, character := range instructions {
+		if string(character) == "(" {
+			currentFloor++
+		} else {
+			currentFloor--
+		}
+
+		// Check for floor -1
+		if currentFloor == -1 {
+			fmt.Printf("The instruction that took Santa to floor -1 was at index %d\n", index+1)
+			break
+		}
+	}
 }
 
 func main() {
 	part1()
+	part2()
 }
