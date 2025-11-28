acd346d0e7601a744c57a8f17586924e97a7ddb1
yearlongmason <mdudelee@gmail.com>
2025-11-28 01:36:34 -0500

    Finished day 3 part 1 in Go

--- 2015/Day03/Day03Code.go
+++ 2015/Day03/Day03Code.go
@@ -3,11 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
-
-type Coordinate struct {
-	x, y int
-}
 
 func getInput(fileName string) string {
 	input, err := os.ReadFile(fileName)
@@ -18,6 +15,35 @@ func getInput(fileName string) string {
 	return string(input)
 }
 
+func moveSanta(character string, x *int, y *int) {
+	// Moves x and y coordinates (pass by reference)
+	switch character {
+	case ">":
+		*x++
+	case "<":
+		*x--
+	case "^":
+		*y++
+	case "v":
+		*y--
+	}
+}
+
+func part1() {
+	// Use a map to keep track of visited coords for speed
+	x, y := 0, 0
+	visitedCoords := make(map[string]bool)
+	visitedCoords["0,0"] = true
+
+	for _, character := range getInput("santaDirections.txt") {
+		// Move Santa and add new coordinates to map
+		moveSanta(string(character), &x, &y)
+		visitedCoords[strconv.Itoa(x)+","+strconv.Itoa(y)] = true
+	}
+
+	fmt.Printf("Number of houses visited at least once: %d\n", len(visitedCoords))
+}
+
 func main() {
-	fmt.Println(getInput("santaDirections.txt"))
+	part1()
 }
