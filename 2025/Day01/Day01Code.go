bec09d2eebbde1a18402e699514cb4dba91b112c
yearlongmason <mdudelee@gmail.com>
2025-12-01 11:05:16 -0500

    Finished part 1 Go

---
+++ 2025/Day01/Day01Code.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Instruction struct {
+	direction string
+	rotations int
+}
+
+func getInput(fileName string) string {
+	input, err := os.ReadFile(fileName)
+	if err != nil {
+		fmt.Printf("Could not read file: %s\n", fileName)
+	}
+
+	return string(input)
+}
+
+func getRotations() []int {
+	// Create a slice of ints from the input file that get the number of rotations
+	// Positive number if the direction is right, negative if the direction is left
+	instructions := make([]int, 0)
+	for currentInstruction := range strings.SplitSeq(getInput("instructions.txt"), "\n") {
+		direction := string(currentInstruction[0])
+		rotations, _ := strconv.Atoi(strings.TrimSpace(currentInstruction[1:]))
+		if direction == "R" {
+			instructions = append(instructions, rotations)
+		} else {
+			instructions = append(instructions, rotations*-1)
+		}
+	}
+	return instructions
+}
+
+func part1() {
+	numZeros := 0
+	dial := 50
+	// Loop through each rotation
+	for _, rotations := range getRotations() {
+		// Move the dial and account for overflow/underflow
+		dial += rotations
+		dial %= 100
+		if dial == 0 {
+			numZeros++
+		}
+	}
+
+	fmt.Printf("Number of times the dial lands on 0: %d\n", numZeros)
+}
+
+func main() {
+	part1()
+}
