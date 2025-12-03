ea4d8f4a6e0e3c42df750582067fe45d54500df0
yearlongmason <mdudelee@gmail.com>
2025-12-03 15:26:46 -0500

    Implemented recursive part 2 Go

--- 2025/Day03/Day03Code.go
+++ 2025/Day03/Day03Code.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"slices"
 	"strconv"
@@ -42,6 +43,30 @@ func part1() {
 	fmt.Printf("Total output joltage: %d\n", totalOutputJoltage)
 }
 
+func getJoltage(bank string, digitsRemaining int, remainingBankStart int) int {
+	bankSlice := stringToIntSlice(bank)
+	// Base case: there's just one digit left so return the highest one left
+	if digitsRemaining == 1 {
+		return slices.Max(bankSlice[remainingBankStart:])
+	}
+
+	// Get the next digit and it's next index
+	nextDigit := slices.Max(bankSlice[remainingBankStart : len(bank)-digitsRemaining+1])
+	remainingBankStart = slices.Index(bankSlice[remainingBankStart:len(bank)-digitsRemaining+1], nextDigit) + remainingBankStart
+	// Get current battery joltage (E.g. nextDigit = 9 and digitsRemaining = 5 then currentBatteryJoltage = 90000)
+	currentBatteryJoltage := nextDigit * int(math.Pow(10, float64(digitsRemaining-1)))
+	return currentBatteryJoltage + getJoltage(bank, digitsRemaining-1, remainingBankStart+1)
+}
+
+func part2() {
+	totalOutputJoltage := 0
+	for bank := range strings.SplitSeq(getInput("banks.txt"), "\n") {
+		totalOutputJoltage += getJoltage(strings.TrimSpace(bank), 12, 0)
+	}
+	fmt.Printf("Total output joltage: %d\n", totalOutputJoltage)
+}
+
 func main() {
 	part1()
+	part2()
 }
