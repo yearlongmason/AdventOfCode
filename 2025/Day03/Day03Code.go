11ab2b1a64967f284d8a32965edf16ce0575af65
yearlongmason <mdudelee@gmail.com>
2025-12-03 14:22:10 -0500

    Finished day 3 part 1 Go

--- 2025/Day03/Day03Code.go
+++ 2025/Day03/Day03Code.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
+	"slices"
+	"strconv"
+	"strings"
 )
 
 func getInput(fileName string) string {
@@ -14,8 +17,29 @@ func getInput(fileName string) string {
 	return string(input)
 }
 
+func stringToIntSlice(str string) []int {
+	// Convert bank (string of digits) into a slice of ints
+	intSlice := make([]int, 0)
+	for _, digit := range str {
+		digit, err := strconv.Atoi(strings.TrimSpace(string(digit)))
+		if err == nil {
+			intSlice = append(intSlice, digit)
+		}
+	}
+	return intSlice
+}
+
 func part1() {
-	fmt.Println(getInput("banks.txt"))
+	totalOutputJoltage := 0
+	for bank := range strings.SplitSeq(getInput("banks.txt"), "\n") {
+		bankSlice := stringToIntSlice(bank)
+		// Find first and second digit and add to total output joltage
+		firstDigit := slices.Max(bankSlice[:len(bankSlice)-1])
+		secondDigit := slices.Max(bankSlice[slices.Index(bankSlice, firstDigit)+1:])
+		totalOutputJoltage += (firstDigit * 10) + secondDigit
+	}
+
+	fmt.Printf("Total output joltage: %d\n", totalOutputJoltage)
 }
 
 func main() {
